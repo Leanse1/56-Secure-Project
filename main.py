036367302163373e8ff76279372593a50d5fdb22
Leanse Sangeerth <leansesection@gmail.com>
2024-02-01 17:35:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+# import json
 import streamlit as st
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from PIL import Image
 import numpy as np
-
 from util import classify, set_background
+# import streamlit_lottie as st_lottie
 
 
-set_background('./bgs/bg.png')
 
+set_background('./bgs/cloud.jpg')
 # upload file
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
-
 # load classifier
-model = load_model('./model/56_secure.h5')
+model = load_model('./model/cloud.h5')
 
 # load class names
 with open('./model/labels.txt', 'r') as f:
@@ -22,10 +22,8 @@ with open('./model/labels.txt', 'r') as f:
 if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
-
     # classify image
     class_name, conf_score = classify(image, model, class_names)
-
     # write classification
-    st.markdown("<h2 style='color: white;'>{}</h2>".format(class_name), unsafe_allow_html=True)
-    st.markdown("<h3 style='color: white;'>Score: {}%</h3>".format(int(conf_score * 1000) / 10), unsafe_allow_html=True)
+    st.markdown("<h2 style='color: black;'>{}</h2>".format(class_name), unsafe_allow_html=True)
+    st.markdown("<h3 style='color: black;'>Score: {}%</h3>".format(int(conf_score * 1000) / 10), unsafe_allow_html=True)
